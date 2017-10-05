c83cfb4935ccea507fc9259ea30b5f5242db2011
Ahmed Masud <ahmed.masud@trustifier.com>
2017-10-05 16:56:40 -0400

    Initial commit

---
+++ fs/aifs/file.c
@@ -0,0 +1,381 @@
+/*
+ * Copyright (c) 1998-2017 Erez Zadok
+ * Copyright (c) 2009	   Shrikar Archak
+ * Copyright (c) 2003-2017 Stony Brook University
+ * Copyright (c) 2003-2017 The Research Foundation of SUNY
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include "aifs.h"
+
+static ssize_t aifs_read(struct file *file, char __user *buf,
+			   size_t count, loff_t *ppos)
+{
+	int err;
+	struct file *lower_file;
+	struct dentry *dentry = file->f_path.dentry;
+
+	lower_file = aifs_lower_file(file);
+	err = vfs_read(lower_file, buf, count, ppos);
+	/* update our inode atime upon a successful lower read */
+	if (err >= 0)
+		fsstack_copy_attr_atime(d_inode(dentry),
+					file_inode(lower_file));
+
+	return err;
+}
+
+static ssize_t aifs_write(struct file *file, const char __user *buf,
+			    size_t count, loff_t *ppos)
+{
+	int err;
+
+	struct file *lower_file;
+	struct dentry *dentry = file->f_path.dentry;
+
+	lower_file = aifs_lower_file(file);
+	err = vfs_write(lower_file, buf, count, ppos);
+	/* update our inode times+sizes upon a successful lower write */
+	if (err >= 0) {
+		fsstack_copy_inode_size(d_inode(dentry),
+					file_inode(lower_file));
+		fsstack_copy_attr_times(d_inode(dentry),
+					file_inode(lower_file));
+	}
+
+	return err;
+}
+
+static int aifs_readdir(struct file *file, struct dir_context *ctx)
+{
+	int err;
+	struct file *lower_file = NULL;
+	struct dentry *dentry = file->f_path.dentry;
+
+	lower_file = aifs_lower_file(file);
+	err = iterate_dir(lower_file, ctx);
+	file->f_pos = lower_file->f_pos;
+	if (err >= 0)		/* copy the atime */
+		fsstack_copy_attr_atime(d_inode(dentry),
+					file_inode(lower_file));
+	return err;
+}
+
+static long aifs_unlocked_ioctl(struct file *file, unsigned int cmd,
+				  unsigned long arg)
+{
+	long err = -ENOTTY;
+	struct file *lower_file;
+
+	lower_file = aifs_lower_file(file);
+
+	/* XXX: use vfs_ioctl if/when VFS exports it */
+	if (!lower_file || !lower_file->f_op)
+		goto out;
+	if (lower_file->f_op->unlocked_ioctl)
+		err = lower_file->f_op->unlocked_ioctl(lower_file, cmd, arg);
+
+	/* some ioctls can change inode attributes (EXT2_IOC_SETFLAGS) */
+	if (!err)
+		fsstack_copy_attr_all(file_inode(file),
+				      file_inode(lower_file));
+out:
+	return err;
+}
+
+#ifdef CONFIG_COMPAT
+static long aifs_compat_ioctl(struct file *file, unsigned int cmd,
+				unsigned long arg)
+{
+	long err = -ENOTTY;
+	struct file *lower_file;
+
+	lower_file = aifs_lower_file(file);
+
+	/* XXX: use vfs_ioctl if/when VFS exports it */
+	if (!lower_file || !lower_file->f_op)
+		goto out;
+	if (lower_file->f_op->compat_ioctl)
+		err = lower_file->f_op->compat_ioctl(lower_file, cmd, arg);
+
+out:
+	return err;
+}
+#endif
+
+static int aifs_mmap(struct file *file, struct vm_area_struct *vma)
+{
+	int err = 0;
+	bool willwrite;
+	struct file *lower_file;
+	const struct vm_operations_struct *saved_vm_ops = NULL;
+
+	/* this might be deferred to mmap's writepage */
+	willwrite = ((vma->vm_flags | VM_SHARED | VM_WRITE) == vma->vm_flags);
+
+	/*
+	 * File systems which do not implement ->writepage may use
+	 * generic_file_readonly_mmap as their ->mmap op.  If you call
+	 * generic_file_readonly_mmap with VM_WRITE, you'd get an -EINVAL.
+	 * But we cannot call the lower ->mmap op, so we can't tell that
+	 * writeable mappings won't work.  Therefore, our only choice is to
+	 * check if the lower file system supports the ->writepage, and if
+	 * not, return EINVAL (the same error that
+	 * generic_file_readonly_mmap returns in that case).
+	 */
+	lower_file = aifs_lower_file(file);
+	if (willwrite && !lower_file->f_mapping->a_ops->writepage) {
+		err = -EINVAL;
+		printk(KERN_ERR "aifs: lower file system does not "
+		       "support writeable mmap\n");
+		goto out;
+	}
+
+	/*
+	 * find and save lower vm_ops.
+	 *
+	 * XXX: the VFS should have a cleaner way of finding the lower vm_ops
+	 */
+	if (!AIFS_F(file)->lower_vm_ops) {
+		err = lower_file->f_op->mmap(lower_file, vma);
+		if (err) {
+			printk(KERN_ERR "aifs: lower mmap failed %d\n", err);
+			goto out;
+		}
+		saved_vm_ops = vma->vm_ops; /* save: came from lower ->mmap */
+	}
+
+	/*
+	 * Next 3 lines are all I need from generic_file_mmap.  I definitely
+	 * don't want its test for ->readpage which returns -ENOEXEC.
+	 */
+	file_accessed(file);
+	vma->vm_ops = &aifs_vm_ops;
+
+	file->f_mapping->a_ops = &aifs_aops; /* set our aops */
+	if (!AIFS_F(file)->lower_vm_ops) /* save for our ->fault */
+		AIFS_F(file)->lower_vm_ops = saved_vm_ops;
+
+out:
+	return err;
+}
+
+static int aifs_open(struct inode *inode, struct file *file)
+{
+	int err = 0;
+	struct file *lower_file = NULL;
+	struct path lower_path;
+
+	/* don't open unhashed/deleted files */
+	if (d_unhashed(file->f_path.dentry)) {
+		err = -ENOENT;
+		goto out_err;
+	}
+
+	file->private_data =
+		kzalloc(sizeof(struct aifs_file_info), GFP_KERNEL);
+	if (!AIFS_F(file)) {
+		err = -ENOMEM;
+		goto out_err;
+	}
+
+	/* open lower object and link aifs's file struct to lower's */
+	aifs_get_lower_path(file->f_path.dentry, &lower_path);
+	lower_file = dentry_open(&lower_path, file->f_flags, current_cred());
+	path_put(&lower_path);
+	if (IS_ERR(lower_file)) {
+		err = PTR_ERR(lower_file);
+		lower_file = aifs_lower_file(file);
+		if (lower_file) {
+			aifs_set_lower_file(file, NULL);
+			fput(lower_file); /* fput calls dput for lower_dentry */
+		}
+	} else {
+		aifs_set_lower_file(file, lower_file);
+	}
+
+	if (err)
+		kfree(AIFS_F(file));
+	else
+		fsstack_copy_attr_all(inode, aifs_lower_inode(inode));
+out_err:
+	return err;
+}
+
+static int aifs_flush(struct file *file, fl_owner_t id)
+{
+	int err = 0;
+	struct file *lower_file = NULL;
+
+	lower_file = aifs_lower_file(file);
+	if (lower_file && lower_file->f_op && lower_file->f_op->flush) {
+		filemap_write_and_wait(file->f_mapping);
+		err = lower_file->f_op->flush(lower_file, id);
+	}
+
+	return err;
+}
+
+/* release all lower object references & free the file info structure */
+static int aifs_file_release(struct inode *inode, struct file *file)
+{
+	struct file *lower_file;
+
+	lower_file = aifs_lower_file(file);
+	if (lower_file) {
+		aifs_set_lower_file(file, NULL);
+		fput(lower_file);
+	}
+
+	kfree(AIFS_F(file));
+	return 0;
+}
+
+static int aifs_fsync(struct file *file, loff_t start, loff_t end,
+			int datasync)
+{
+	int err;
+	struct file *lower_file;
+	struct path lower_path;
+	struct dentry *dentry = file->f_path.dentry;
+
+	err = __generic_file_fsync(file, start, end, datasync);
+	if (err)
+		goto out;
+	lower_file = aifs_lower_file(file);
+	aifs_get_lower_path(dentry, &lower_path);
+	err = vfs_fsync_range(lower_file, start, end, datasync);
+	aifs_put_lower_path(dentry, &lower_path);
+out:
+	return err;
+}
+
+static int aifs_fasync(int fd, struct file *file, int flag)
+{
+	int err = 0;
+	struct file *lower_file = NULL;
+
+	lower_file = aifs_lower_file(file);
+	if (lower_file->f_op && lower_file->f_op->fasync)
+		err = lower_file->f_op->fasync(fd, lower_file, flag);
+
+	return err;
+}
+
+/*
+ * Wrapfs cannot use generic_file_llseek as ->llseek, because it would
+ * only set the offset of the upper file.  So we have to implement our
+ * own method to set both the upper and lower file offsets
+ * consistently.
+ */
+static loff_t aifs_file_llseek(struct file *file, loff_t offset, int whence)
+{
+	int err;
+	struct file *lower_file;
+
+	err = generic_file_llseek(file, offset, whence);
+	if (err < 0)
+		goto out;
+
+	lower_file = aifs_lower_file(file);
+	err = generic_file_llseek(lower_file, offset, whence);
+
+out:
+	return err;
+}
+
+/*
+ * Wrapfs read_iter, redirect modified iocb to lower read_iter
+ */
+ssize_t
+aifs_read_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+	int err;
+	struct file *file = iocb->ki_filp, *lower_file;
+
+	lower_file = aifs_lower_file(file);
+	if (!lower_file->f_op->read_iter) {
+		err = -EINVAL;
+		goto out;
+	}
+
+	get_file(lower_file); /* prevent lower_file from being released */
+	iocb->ki_filp = lower_file;
+	err = lower_file->f_op->read_iter(iocb, iter);
+	iocb->ki_filp = file;
+	fput(lower_file);
+	/* update upper inode atime as needed */
+	if (err >= 0 || err == -EIOCBQUEUED)
+		fsstack_copy_attr_atime(d_inode(file->f_path.dentry),
+					file_inode(lower_file));
+out:
+	return err;
+}
+
+/*
+ * Wrapfs write_iter, redirect modified iocb to lower write_iter
+ */
+ssize_t
+aifs_write_iter(struct kiocb *iocb, struct iov_iter *iter)
+{
+	int err;
+	struct file *file = iocb->ki_filp, *lower_file;
+
+	lower_file = aifs_lower_file(file);
+	if (!lower_file->f_op->write_iter) {
+		err = -EINVAL;
+		goto out;
+	}
+
+	get_file(lower_file); /* prevent lower_file from being released */
+	iocb->ki_filp = lower_file;
+	err = lower_file->f_op->write_iter(iocb, iter);
+	iocb->ki_filp = file;
+	fput(lower_file);
+	/* update upper inode times/sizes as needed */
+	if (err >= 0 || err == -EIOCBQUEUED) {
+		fsstack_copy_inode_size(d_inode(file->f_path.dentry),
+					file_inode(lower_file));
+		fsstack_copy_attr_times(d_inode(file->f_path.dentry),
+					file_inode(lower_file));
+	}
+out:
+	return err;
+}
+
+const struct file_operations aifs_main_fops = {
+	.llseek		= generic_file_llseek,
+	.read		= aifs_read,
+	.write		= aifs_write,
+	.unlocked_ioctl	= aifs_unlocked_ioctl,
+#ifdef CONFIG_COMPAT
+	.compat_ioctl	= aifs_compat_ioctl,
+#endif
+	.mmap		= aifs_mmap,
+	.open		= aifs_open,
+	.flush		= aifs_flush,
+	.release	= aifs_file_release,
+	.fsync		= aifs_fsync,
+	.fasync		= aifs_fasync,
+	.read_iter	= aifs_read_iter,
+	.write_iter	= aifs_write_iter,
+};
+
+/* trimmed directory options */
+const struct file_operations aifs_dir_fops = {
+	.llseek		= aifs_file_llseek,
+	.read		= generic_read_dir,
+	.iterate	= aifs_readdir,
+	.unlocked_ioctl	= aifs_unlocked_ioctl,
+#ifdef CONFIG_COMPAT
+	.compat_ioctl	= aifs_compat_ioctl,
+#endif
+	.open		= aifs_open,
+	.release	= aifs_file_release,
+	.flush		= aifs_flush,
+	.fsync		= aifs_fsync,
+	.fasync		= aifs_fasync,
+};
---
+++ fs/aifs/super.c
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 1998-2017 Erez Zadok
+ * Copyright (c) 2009	   Shrikar Archak
+ * Copyright (c) 2003-2017 Stony Brook University
+ * Copyright (c) 2003-2017 The Research Foundation of SUNY
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include "aifs.h"
+
+/*
+ * The inode cache is used with alloc_inode for both our inode info and the
+ * vfs inode.
+ */
+static struct kmem_cache *aifs_inode_cachep;
+
+/* final actions when unmounting a file system */
+static void aifs_put_super(struct super_block *sb)
+{
+	struct aifs_sb_info *spd;
+	struct super_block *s;
+
+	spd = AIFS_SB(sb);
+	if (!spd)
+		return;
+
+	/* decrement lower super references */
+	s = aifs_lower_super(sb);
+	aifs_set_lower_super(sb, NULL);
+	atomic_dec(&s->s_active);
+
+	kfree(spd);
+	sb->s_fs_info = NULL;
+}
+
+static int aifs_statfs(struct dentry *dentry, struct kstatfs *buf)
+{
+	int err;
+	struct path lower_path;
+
+	aifs_get_lower_path(dentry, &lower_path);
+	err = vfs_statfs(&lower_path, buf);
+	aifs_put_lower_path(dentry, &lower_path);
+
+	/* set return buf to our f/s to avoid confusing user-level utils */
+	buf->f_type = AIFS_SUPER_MAGIC;
+
+	return err;
+}
+
+/*
+ * @flags: numeric mount options
+ * @options: mount options string
+ */
+static int aifs_remount_fs(struct super_block *sb, int *flags, char *options)
+{
+	int err = 0;
+
+	/*
+	 * The VFS will take care of "ro" and "rw" flags among others.  We
+	 * can safely accept a few flags (RDONLY, MANDLOCK), and honor
+	 * SILENT, but anything else left over is an error.
+	 */
+	if ((*flags & ~(MS_RDONLY | MS_MANDLOCK | MS_SILENT)) != 0) {
+		printk(KERN_ERR
+		       "aifs: remount flags 0x%x unsupported\n", *flags);
+		err = -EINVAL;
+	}
+
+	return err;
+}
+
+/*
+ * Called by iput() when the inode reference count reached zero
+ * and the inode is not hashed anywhere.  Used to clear anything
+ * that needs to be, before the inode is completely destroyed and put
+ * on the inode free list.
+ */
+static void aifs_evict_inode(struct inode *inode)
+{
+	struct inode *lower_inode;
+
+	truncate_inode_pages(&inode->i_data, 0);
+	clear_inode(inode);
+	/*
+	 * Decrement a reference to a lower_inode, which was incremented
+	 * by our read_inode when it was created initially.
+	 */
+	lower_inode = aifs_lower_inode(inode);
+	aifs_set_lower_inode(inode, NULL);
+	iput(lower_inode);
+}
+
+static struct inode *aifs_alloc_inode(struct super_block *sb)
+{
+	struct aifs_inode_info *i;
+
+	i = kmem_cache_alloc(aifs_inode_cachep, GFP_KERNEL);
+	if (!i)
+		return NULL;
+
+	/* memset everything up to the inode to 0 */
+	memset(i, 0, offsetof(struct aifs_inode_info, vfs_inode));
+
+	i->vfs_inode.i_version = 1;
+	return &i->vfs_inode;
+}
+
+static void aifs_destroy_inode(struct inode *inode)
+{
+	kmem_cache_free(aifs_inode_cachep, AIFS_I(inode));
+}
+
+/* aifs inode cache constructor */
+static void init_once(void *obj)
+{
+	struct aifs_inode_info *i = obj;
+
+	inode_init_once(&i->vfs_inode);
+}
+
+int aifs_init_inode_cache(void)
+{
+	int err = 0;
+
+	aifs_inode_cachep =
+		kmem_cache_create("aifs_inode_cache",
+				  sizeof(struct aifs_inode_info), 0,
+				  SLAB_RECLAIM_ACCOUNT, init_once);
+	if (!aifs_inode_cachep)
+		err = -ENOMEM;
+	return err;
+}
+
+/* aifs inode cache destructor */
+void aifs_destroy_inode_cache(void)
+{
+	if (aifs_inode_cachep)
+		kmem_cache_destroy(aifs_inode_cachep);
+}
+
+/*
+ * Used only in nfs, to kill any pending RPC tasks, so that subsequent
+ * code can actually succeed and won't leave tasks that need handling.
+ */
+static void aifs_umount_begin(struct super_block *sb)
+{
+	struct super_block *lower_sb;
+
+	lower_sb = aifs_lower_super(sb);
+	if (lower_sb && lower_sb->s_op && lower_sb->s_op->umount_begin)
+		lower_sb->s_op->umount_begin(lower_sb);
+}
+
+const struct super_operations aifs_sops = {
+	.put_super	= aifs_put_super,
+	.statfs		= aifs_statfs,
+	.remount_fs	= aifs_remount_fs,
+	.evict_inode	= aifs_evict_inode,
+	.umount_begin	= aifs_umount_begin,
+	.show_options	= generic_show_options,
+	.alloc_inode	= aifs_alloc_inode,
+	.destroy_inode	= aifs_destroy_inode,
+	.drop_inode	= generic_delete_inode,
+};
+
+/* NFS support */
+
+static struct inode *aifs_nfs_get_inode(struct super_block *sb, u64 ino,
+					  u32 generation)
+{
+	struct super_block *lower_sb;
+	struct inode *inode;
+	struct inode *lower_inode;
+
+	lower_sb = aifs_lower_super(sb);
+	lower_inode = ilookup(lower_sb, ino);
+	inode = aifs_iget(sb, lower_inode);
+	return inode;
+}
+
+static struct dentry *aifs_fh_to_dentry(struct super_block *sb,
+					  struct fid *fid, int fh_len,
+					  int fh_type)
+{
+	return generic_fh_to_dentry(sb, fid, fh_len, fh_type,
+				    aifs_nfs_get_inode);
+}
+
+static struct dentry *aifs_fh_to_parent(struct super_block *sb,
+					  struct fid *fid, int fh_len,
+					  int fh_type)
+{
+	return generic_fh_to_parent(sb, fid, fh_len, fh_type,
+				    aifs_nfs_get_inode);
+}
+
+/*
+ * all other funcs are default as defined in exportfs/expfs.c
+ */
+
+const struct export_operations aifs_export_ops = {
+	.fh_to_dentry	   = aifs_fh_to_dentry,
+	.fh_to_parent	   = aifs_fh_to_parent
+};
